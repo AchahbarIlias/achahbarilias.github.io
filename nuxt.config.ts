5f83efe771592d2af63c595261ae512e8767ff7a
AchahbarIlias <ilias.achahbar@outlook.com>
2026-07-06 15:45:57 +0200

    fix: prune

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: false },
-
   app: {
     baseURL: "/",
-    buildAssetsDir: "/_nuxt/",
   },
 
   css: ["~/assets/css/main.css"],
 
-  modules: [
-    '@nuxt/content',
-  ],
-
   vite: {
-    optimizeDeps: {
-      include: []
-    },
-    plugins: [
-      tailwindcss(),
-    ]
-  },
-
-  imports: {
-    presets: [
-      {
-        from: 'content',
-        imports: ['content'],
-      },
-    ],
+    plugins: [tailwindcss()],
   },
 
   nitro: {
-    preset: 'github_pages'
-  }
-})
+    preset: "github_pages",
+  },
+});
