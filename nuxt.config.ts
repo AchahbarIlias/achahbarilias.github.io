689cb1808b7d226cec22d70294ce5ea36bb819b9
AchahbarIlias <ilias.achahbar@outlook.com>
2026-07-06 13:55:46 +0200

    fix: css path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
 
-  css: [
-    '~/assets/main.css'
-  ],
+  css: ['./app/assets/css/main.css'],
 
   modules: [
     '@nuxt/content',
