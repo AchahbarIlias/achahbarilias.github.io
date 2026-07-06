1a181fa6e3dcaa89b770db6a37800551f8900cd2
AchahbarIlias <ilias.achahbar@outlook.com>
2026-07-06 11:50:19 +0200

    fix: added env pathing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
 
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+  },
+
   css: [
     '~/assets/main.css'
   ],
